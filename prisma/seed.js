515755f7b363aed882e88555adb6d3d619d4ff14
KYEONGMIN CHO <ggingmin@gmail.com>
2023-03-24 13:54:05 +0900

    package.json, seeds.js edited

--- prisma/seed.js
+++ prisma/seed.js
@@ -4,46 +4,14 @@ const prisma = new PrismaClient()
 
 const userData = [
   {
-    name: 'Alice',
-    email: 'alice@prisma.io',
+    name: 'test',
+    email: 'test@cloudtype.co.kr',
     posts: {
       create: [
         {
-          title: 'Join the Prisma Slack',
-          content: 'https://slack.prisma.io',
+          title: 'Cloudtype: Prisma-Next Example',
+          content: 'Cloudtype: Prisma-Next Example',
           published: true,
-        },
-      ],
-    },
-  },
-  {
-    name: 'Nilu',
-    email: 'nilu@prisma.io',
-    posts: {
-      create: [
-        {
-          title: 'Follow Prisma on Twitter',
-          content: 'https://www.twitter.com/prisma',
-          published: true,
-          viewCount: 42,
-        },
-      ],
-    },
-  },
-  {
-    name: 'Mahmoud',
-    email: 'mahmoud@prisma.io',
-    posts: {
-      create: [
-        {
-          title: 'Ask a question about Prisma on GitHub',
-          content: 'https://www.github.com/prisma/prisma/discussions',
-          published: true,
-          viewCount: 128,
-        },
-        {
-          title: 'Prisma on YouTube',
-          content: 'https://pris.ly/youtube',
         },
       ],
     },
